a10d5014c3b1ff0170947a9f138d20cc83f831d2
Fabien Lavocat <flavocat@radiantlogic.com>
2015-09-15 10:04:38 -0700

    Initial Project creation

---
+++ MVC Owin WsFederation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Owin_WsFederation.Startup))]
+namespace MVC_Owin_WsFederation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
